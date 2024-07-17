7fcc27619c88a6e5c95f0b44f4dad9ecd7090cff
kingksjo <121252722+kingksjo@users.noreply.github.com>
2024-07-17 19:14:05 +0100

    Add files via upload

---
+++ src/hooks/useCounterContract.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import Counter from 'c:/Users/USER/Documents/TG Mini App/me-twa/src/contracts/Counter.ts';
+import { useTonClient } from './useTonClient';
+import { useAsyncInitialize } from './useAsyncInitialize';
+import { useTonConnect } from './useTonConnect';
+import { Address, OpenedContract } from '@ton/core';
+
+export function useCounterContract() {
+  const client = useTonClient();
+  const [val, setVal] = useState<null | string>();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+
+  const counterContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new Counter(
+      Address.parse('EQCzFPg_CTw4bw-zYvNCSrdtrfZGi1R8_cY2Nl1dXPkyGAYN') // replace with your address from tutorial 2 step 8
+    );
+    return client.open(contract) as OpenedContract<Counter>;
+  }, [client]);
+
+  useEffect(() => {
+    async function getValue() {
+      if (!counterContract) return;
+      setVal(null);
+      const val = await counterContract.getCounter();
+      setVal(val.toString());
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
+    }
+    getValue();
+  }, [counterContract]);
+
+  return {
+    value: val,
+    address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrement(sender);
+    },
+  };
+}
